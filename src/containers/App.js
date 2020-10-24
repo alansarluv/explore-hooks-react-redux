60de55115979f070a602cca016555eadb67be6ed
alansarluv <alan.sarluf@gmail.com>
2020-10-24 22:41:41 +0700

    Update to react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import '../containers/App.css';
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchField: ''
-    }
-  }
-  componentDidMount() {
+function App() {
+
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const [count, setCount] = useState(0);
+
+  useEffect( () => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({robots: users})
+      .then(users => setRobots(users)
+    );
+  },[count]) // onnly run if count changes
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
+  }
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+  if (robots.length === 0) {
+    return <h1>Loading...</h1>
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="f1">Robofriends</h1>
+        <button onClick={()=> setCount(count+1)}>Click Me! {count}</button>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots}/>
+          </ErrorBoundry>
+        </Scroll>
+      </div>
     );
   }
-  onSearchChange = (event) => {
-    this.setState({searchField: event.target.value});
-  }
-  render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-    });
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1">Robofriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <ErrorBoundry>
-              <CardList robots={filteredRobots}/>
-            </ErrorBoundry>
-          </Scroll>
-        </div>
-      );
-    }
-  } 
 }
 
 export default App;
