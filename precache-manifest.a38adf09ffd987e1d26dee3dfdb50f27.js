c1152cc9828929807d49d3647a2b4b41b5b985c9
alansarluv <alan.sarluf@gmail.com>
2020-10-24 15:36:02 +0700

    Updates

---
+++ precache-manifest.a38adf09ffd987e1d26dee3dfdb50f27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a7584286da60822aee86fb9432c2fd",
+    "url": "/robofriends-learn-react/index.html"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/robofriends-learn-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "93a277483563ec62e9fc",
+    "url": "/robofriends-learn-react/static/css/main.75eadf99.chunk.css"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/robofriends-learn-react/static/js/2.3788da92.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robofriends-learn-react/static/js/2.3788da92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a277483563ec62e9fc",
+    "url": "/robofriends-learn-react/static/js/main.4bf8e7f0.chunk.js"
+  },
+  {
+    "revision": "71f366b34306e73c3b1a",
+    "url": "/robofriends-learn-react/static/js/runtime-main.9ac9c86d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-learn-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
