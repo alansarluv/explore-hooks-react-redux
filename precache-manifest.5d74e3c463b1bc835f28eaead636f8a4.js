572e1c0e16759937497eaad603d935f39c3998d9
alansarluv <alan.sarluf@gmail.com>
2020-10-24 16:17:56 +0700

    Updates

---
+++ precache-manifest.5d74e3c463b1bc835f28eaead636f8a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f34aa90937c71c0f5d3ea2a78659de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8610d8f2b302fa690e95",
+    "url": "/static/css/main.3f4be7fc.chunk.css"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/static/js/2.3788da92.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3788da92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8610d8f2b302fa690e95",
+    "url": "/static/js/main.971b4143.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
