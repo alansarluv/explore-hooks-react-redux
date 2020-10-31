bf947b3851814a6ec8c2920e81f24d6ace23f9ff
alansarluv <alan.sarluf@gmail.com>
2020-11-01 00:15:14 +0700

    Setup async redux hooks & combineReducers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
@@ -6,32 +6,33 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import '../containers/App.css';
 
-import { setSearchField } from '../actions';
+import { requestRobot, setSearchField } from '../actions';
 
-function App(props) {
+function App() {
 
-  const [robots, setRobots] = useState([]);
-  const [count, setCount] = useState(0);
-  const searchField = useSelector( state => state.searchField ); // hooks redux
+  const searchField = useSelector( state => state.searchRobots.searchField ); // hooks redux
+  const robots = useSelector( state => state.requestRobots.robots ); // hooks redux - thunk middleware
+  const isPending = useSelector( state => state.requestRobots.isPending ); // hooks redux - thunk middleware
+  const error = useSelector( state => state.requestRobots.error ); // hooks redux - thunk middleware
   const dispatch = useDispatch(); // hooks redux
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => setRobots(users)
-    );
-  },[count]) // only run if count changes
+  
   const onSearchChange = (event) => dispatch(setSearchField(event.target.value)); // hooks redux
+  
   const filteredRobots = robots.filter(robot => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase());
   });
-  if (robots.length === 0) {
+  useEffect(() => {
+    dispatch(requestRobot());
+  },[dispatch]);
+
+  if (isPending) {
     return <h1>Loading...</h1>
+  } else if (error) {
+    return <p>{error}</p>
   } else {
     return (
       <div className="tc">
         <h1 className="f1">Robofriends</h1>
-        <button onClick={()=> setCount(count+1)}>Click Me! {count}</button>
         <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundry>
--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,15 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from './constants.js';
 
-const initialState = {
+const initialStateSearch = {
   searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, { searchField: action.payload });
@@ -13,3 +18,21 @@ export const searchRobots = (state=initialState, action={}) => {
   }
 }
 
+const initialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: ''
+}
+
+export const requestRobots = (state=initialStateRobots, action={}) => {
+  switch(action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, { isPending: true });
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, { isPending: false, robots: action.payload });
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, { isPending: false, error: action.payload });
+    default: 
+      return state;
+  }
+}
